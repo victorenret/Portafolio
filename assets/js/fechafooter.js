b954f90ee0980a370aabba2b1abb8efa564af1d8
Victor Retamal <victorenret@gmail.com>
2022-02-18 00:13:10 -0300

    Correccion Final

--- assets/js/fechafooter.js
+++
@@ -1,6 +0,0 @@
-
-//FECHA PARA EL FOOTER DE LA PAGINA
-
-var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-var f = new Date();
-document.write(meses[f.getMonth()] + " de " + f.getFullYear());
